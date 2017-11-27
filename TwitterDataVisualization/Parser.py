ff83bab3d7fd7c39de22578706b8daf6601fa63f
Bala Vineeth Netha Thatipamula <balavnth@gmail.com>
2017-11-27 13:33:19 -0500

    Added Comments

--- TwitterDataVisualization/Parser.py
+++ TwitterDataVisualization/Parser.py
@@ -1,34 +1,40 @@
+#Writes all the live streaming tweets into this JSON file
 file_handler = open("ClemVSWake.json")
 
+'''
+Below are the Lists of different names a Player/Coach can we addressed in Twitter
+'''
+#Clemson QB
 clemson_qb = ["Deshaun Watson", "Deshaun", "deshaun"]
-#fsu_coach = ["Jimbo Fisher", "jimbo", "Jimbo"]
+#Clemson Head Coach
 clemson_coach = ['Dabo','Dabo Swinney']
-#fsu_qb = ['Deondre Francois', 'Deondre', 'deondre']
-Wake_qb = ['Hinton', 'hinton', 'Kendall Hinton']
+#FSU QB
+fsu_qb = ['Deondre Francois', 'Deondre', 'deondre']
+#FSU Head Coach
+fsu_coach = ["Jimbo Fisher", "jimbo", "Jimbo"]
 
-lines_count = 0
+#Getting the count of Each Player/Coach
+tweet_count = 0
 clemson_qb_count = 0
-#fsu_qb_count = 0
+fsu_qb_count = 0
 clemson_coach_count = 0
-#fsu_coach_count = 0
-Wake_qb_count = 0
+fsu_coach_count = 0
 for line in file_handler:
-	lines_count = lines_count + 1
+	tweet_count = tweet_count + 1
 	if any(x in line for x in clemson_qb):
 		clemson_qb_count = clemson_qb_count+1
-	'''if any(x in line for x in fsu_qb):
-		fsu_qb_count = fsu_qb_count + 1'''
+	if any(x in line for x in fsu_qb):
+		fsu_qb_count = fsu_qb_count + 1
 	if any(x in line for x in clemson_coach):
 		clemson_coach_count = clemson_coach_count + 1
-	'''if any(x in line for x in fsu_coach):
-		fsu_coach_count = fsu_coach_count + 1'''
+	if any(x in line for x in fsu_coach):
+		fsu_coach_count = fsu_coach_count + 1
 	if any(x in line for x in Wake_qb):
 		Wake_qb_count = Wake_qb_count + 1
 
 
-print ("Total number of lines: ", lines_count)
+print ("Total tweet count: ", tweet_count)
 print ("Deshaun Watson mentions: ", clemson_qb_count)
-#print ("Jimbo Fisher mentions: ", fsu_coach_count)
+print ("Jimbo Fisher mentions: ", fsu_coach_count)
 print ("Dabo Swinney mentions: ", clemson_coach_count)
-#print ("Deondre Francois mentions: ", fsu_qb_count)
-print ("Kendall Hinton mentions: ", Wake_qb_count)
+print ("Deondre Francois mentions: ", fsu_qb_count)
--- TwitterDataVisualization/Tweets.py
+++ TwitterDataVisualization/Tweets.py
@@ -2,32 +2,36 @@ import tweepy
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
- 
-consumer_key = '0NkHawj0dWMQRtWK7HaHOut1w'
-consumer_secret = 'eBeSwKj0t49DiryQNclbf0FiG1TnO59JmrM8y15U5AkLYnrk1c'
-access_token = '779852978048032768-ldAnwI2g0vME6hflDzkNSJDz0F5AUJO'
-access_secret = 'nMSTNyJbbtvCuXsrinQo6i7jOqJa2Yjk9sIsZv7SmPCPf'
- 
+
+consumer_key = '<consumer key from twitter>'
+consumer_secret = '<consumer secret key from twitter>'
+access_token = '<twitter account access keytoken>'
+access_secret = '<twitter account access secret keytoken>'
+
+# Authenticating and Connecting to Twitter API
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
- 
+
 api = tweepy.API(auth)
 
+# Twitter StreamListener
 class MyListener(StreamListener):
- 
+    # On Getting Twitter Stream Data
     def on_data(self, data):
         try:
+            # Writing into JSON File
             with open('ClemVSCuse.json', 'a') as f:
                 f.write(data)
                 return True
         except BaseException as e:
             print("Error occured "+str(e))
         return True
- 
+
+    # On error show the error message
     def on_error(self, status):
         print(status)
         return True
- 
+
 
 while True:
 	try:

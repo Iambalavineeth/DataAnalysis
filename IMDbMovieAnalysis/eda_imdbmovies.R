8eca42242f3ed7946128063b0a7dbdb2f4eb5f7c
Bala Vineeth Netha Thatipamula <balavnth@gmail.com>
2017-11-27 13:11:54 -0500

    Modified eda file

--- IMDbMovieAnalysis/eda_imdbmovies.R
+++ IMDbMovieAnalysis/eda_imdbmovies.R
@@ -21,21 +21,21 @@ attach(movie_table)
 str(movie_table)
 dim(movie_table)
 
-#Correlaion between the variables 
+#Correlaion between the variables
 numericColumns <- sapply(movie_table, is.numeric)
 corMatrix<-cor(movie_table[,numericColumns])
 corMatrix
 
 #Calculating Average IMDb rating by year relationship
-#temp <- movie_table %>% select(imdb_score,title_year)
-#temp <- temp %>% group_by(title_year)%>% summarise(score=mean(imdb_score))
-#temp <- na.omit(temp)
-#p <- plot_ly(temp, x = title_year, y = score, name = "Avg Score by Year")
-#p %>%
-#  add_trace(y = fitted(loess(score ~ as.numeric(title_year))), x = title_year) %>%
-#  layout(title = "Year and Score", showlegend = FALSE) %>%
-#  dplyr::filter(score == max(score)) %>%
-#  layout(annotations = list(x = title_year, y = score, text = "Peak", showarrow = T))
+temp <- movie_table %>% select(imdb_score,title_year)
+temp <- temp %>% group_by(title_year)%>% summarise(score=mean(imdb_score))
+temp <- na.omit(temp)
+p <- plot_ly(temp, x = title_year, y = score, name = "Avg Score by Year")
+p %>%
+  add_trace(y = fitted(loess(score ~ as.numeric(title_year))), x = title_year) %>%
+  layout(title = "Year and Score", showlegend = FALSE) %>%
+  dplyr::filter(score == max(score)) %>%
+  layout(annotations = list(x = title_year, y = score, text = "Peak", showarrow = T))
 
 
 #IMDb scores vs Content type
@@ -44,9 +44,9 @@ imdbscore_content <- na.omit(imdbscore_content)
 plot_ly(imdbscore_content, x = imdb_score, color = content_rating, type = "box")
 
 
-#p <- ggplot(data = movie_table, aes(x = as.numeric(title_year), y = imdb_score)) +
-#  geom_point(aes(text = paste("Clarity:", clarity))) +
-#  geom_smooth(aes(colour = cut, fill = cut)) + facet_wrap(~ cut)
+p <- ggplot(data = movie_table, aes(x = as.numeric(title_year), y = imdb_score)) +
+  geom_point(aes(text = paste("Clarity:", clarity))) +
+  geom_smooth(aes(colour = cut, fill = cut)) + facet_wrap(~ cut)
 
-#ggplotly(p)
-
+#Plot showing the Movie Title vs IMDb scores 
+ggplotly(p)
